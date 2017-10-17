de679ef757a307d25eab353851969f6fa8325880
KaygoYM <hamchampion@163.com>
2017-10-17 15:58:15 +0800

    Update eight_queen.py

--- eight_queen.py
+++ eight_queen.py
@@ -6,7 +6,7 @@ def queens(num=8,state=()):
             if len(state)==num-1:
                 yield(pos,)
             else:
-                for result in queens(num,state+(pos,)):
+                for result in queens(num,state+(pos,)):#递归
                     yield (pos,)+result
 
 
